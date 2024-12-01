5ff779c20944f8d2572c2cb0b449e2868c717f9c
Tony Samuels <tonysamuels@protonmail.com>
2024-12-01 13:23:06 +0000

    1.2

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use aoc_runner_derive::aoc;
 use atoi_simd::parse;
 
@@ -6,10 +8,69 @@ use atoi_simd::parse;
 // - Using `&str`, `parse` and `str::split` is ~20-30% slower
 #[aoc(day1, part1)]
 pub fn part1(input: &str) -> i32 {
+    let (left, right) = input_handling(input);
+
+    left.into_iter()
+        .zip(right.into_iter())
+        .map(|(left, right)| (left - right).abs())
+        .sum::<i32>()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &str) -> i32 {
+    let (left, right) = input_handling(input);
+    let left = &mut left.into_iter();
+    let right = &mut right.into_iter();
+
+    let mut similarity = 0;
+    let mut curr_left_similarity = 0;
+
+    let mut curr_left = left.next().unwrap();
+    let mut curr_right = right.next().unwrap();
+
+    loop {
+        match curr_left.cmp(&curr_right) {
+            Ordering::Less => {
+                similarity += curr_left * curr_left_similarity;
+
+                if let Some(new_left) = left.next() {
+                    if curr_left != new_left {
+                        curr_left_similarity = 0;
+                    }
+                    curr_left = new_left;
+                } else {
+                    curr_left_similarity = 0;
+                    break;
+                }
+            }
+            Ordering::Greater => {
+                if let Some(new_right) = right.next() {
+                    curr_right = new_right;
+                } else {
+                    break;
+                }
+            }
+            Ordering::Equal => {
+                curr_left_similarity += 1;
+                if let Some(new_right) = right.next() {
+                    curr_right = new_right;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    similarity += curr_left * curr_left_similarity;
+
+    similarity
+}
+
+fn input_handling(input: &str) -> (Vec<i32>, Vec<i32>) {
     let input = input.as_bytes();
 
-    let left = &mut Vec::with_capacity(1_000);
-    let right = &mut Vec::with_capacity(1_000);
+    let mut left = Vec::with_capacity(1_000);
+    let mut right = Vec::with_capacity(1_000);
 
     for line in input.split(|&c| c == b'\n') {
         let iter = &mut line.split(|&c| c == b' ');
@@ -29,25 +90,27 @@ pub fn part1(input: &str) -> i32 {
     left.sort_unstable();
     right.sort_unstable();
 
-    left.iter()
-        .zip(right.iter())
-        .map(|(left, right)| (left - right).abs())
-        .sum::<i32>()
+    (left, right)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::part1;
+    use super::{part1, part2};
 
-    #[test]
-    fn example() {
-        let input = "3   4
+    const INPUT: &str = "3   4
 4   3
 2   5
 1   3
 3   9
 3   3";
 
-        assert_eq!(part1(input), 11);
+    #[test]
+    fn example_p1() {
+        assert_eq!(part1(INPUT), 11);
+    }
+
+    #[test]
+    fn example_p2() {
+        assert_eq!(part2(INPUT), 31)
     }
 }
