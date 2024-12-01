1c9f0ae1a6f34252fc57cf3ab0e4e6e1ded7e597
Tony Samuels <tonysamuels@protonmail.com>
2024-12-01 20:21:38 +0000

    ci fixes

--- benches/day1.rs
+++ benches/day1.rs
@@ -1,14 +1,14 @@
-use codspeed_criterion_compat::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use solution::day1::{part1, part2};
+use aoc_2024::day1::{part1, part2};
 
 fn bench_part1(c: &mut Criterion) {
-    let input = include_str!("./input.txt");
+    let input = include_str!("../input/2024/day1.txt");
     c.bench_function("part1", |b| b.iter(|| part1(black_box(input))));
 }
 
 fn bench_part2(c: &mut Criterion) {
-    let input = include_str!("./input.txt");
+    let input = include_str!("../input/2024/day1.txt");
     c.bench_function("part2", |b| b.iter(|| part2(black_box(input))));
 }
 
