80da9d4f54e6fbbc5152de055e413ef220f11526
Tony Samuels <tonysamuels@protonmail.com>
2024-12-01 21:13:17 +0000

    recommonise input handling?

--- src/day1.rs
+++ src/day1.rs
@@ -14,6 +14,29 @@ const NUM2_START: usize = NUM_DIGIT_COUNT + SEP_CHAR_COUNT;
 
 #[aoc(day1, part1)]
 pub fn part1(input: &str) -> u32 {
+    let (mut left, mut right) = input_handling(input);
+    left.sort_unstable();
+    right.sort_unstable();
+
+    left.into_iter()
+        .zip(right)
+        .map(|(left, right)| (left as i32 - right as i32).abs() as u32)
+        .sum::<u32>()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &str) -> u32 {
+    let (left, right) = input_handling(input);
+
+    let mut map = [0; 10_usize.pow(NUM_DIGIT_COUNT as u32) - 1];
+    for right in right {
+        map[right as usize] += 1;
+    }
+
+    left.into_iter().map(|left| left * map[left as usize]).sum()
+}
+
+fn input_handling(input: &str) -> (Vec<u32>, Vec<u32>) {
     let input = input.as_bytes();
 
     let mut left = Vec::with_capacity(1_000);
@@ -29,38 +52,7 @@ pub fn part1(input: &str) -> u32 {
         right.push(num2);
     }
 
-    left.sort_unstable();
-    right.sort_unstable();
-
-    left.into_iter()
-        .zip(right)
-        .map(|(left, right)| (left as i32 - right as i32).abs() as u32)
-        .sum::<u32>()
-}
-
-#[aoc(day1, part2)]
-pub fn part2(input: &str) -> u32 {
-    let input = input.as_bytes();
-
-    let mut left = [0; DATA_COUNT];
-    let mut right = [0; DATA_COUNT];
-
-    for (index, line) in input.chunks_exact(LINE_LENGTH + 1).enumerate() {
-        // Strip new line character
-        let line = &line[..LINE_LENGTH];
-        let num1: u32 = atoi_simd::parse_pos(&line[..NUM_DIGIT_COUNT]).unwrap();
-        let num2: u32 = atoi_simd::parse_pos(&line[NUM2_START..]).unwrap();
-
-        left[index] = num1;
-        right[index] = num2;
-    }
-
-    let mut map = [0; 10_usize.pow(NUM_DIGIT_COUNT as u32) - 1];
-    for right in right {
-        map[right as usize] += 1;
-    }
-
-    left.into_iter().map(|left| left * map[left as usize]).sum()
+    (left, right)
 }
 
 #[cfg(test)]
