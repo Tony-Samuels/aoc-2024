b340826a7fa2a5eb46f1924b642d330189dbe3d1
Tony Samuels <tonysamuels@protonmail.com>
2024-12-03 16:00:19 +0000

    perf(day3): quick enable check

--- src/day3.rs
+++ src/day3.rs
@@ -156,67 +156,57 @@ pub fn part2(input: &str) -> u32 {
         let mut sum = 0;
         let mut enabled = true;
         for partial_match_pos in MultiIter::new(input) {
-            match &input[partial_match_pos..] {
-                [b'd', b'o', b'(', b')', ..] => {
-                    enabled = true;
+            if enabled {
+                match &input[partial_match_pos..] {
+                    [b'd', b'o', b'n', b'\'', b't', b'(', b')', ..] => {
+                        enabled = false;
+                    }
+                    [b'm', b'u', b'l', b'(', num1 @ b'0'..=b'9', b',', num2 @ b'0'..=b'9', b')', ..] =>
+                    {
+                        sum += p!(num1) * p!(num2);
+                    }
+                    [b'm', b'u', b'l', b'(', num1_1 @ b'0'..=b'9', num1_2 @ b'0'..=b'9', b',', num2 @ b'0'..=b'9', b')', ..] => {
+                        sum += p!(num1_1, num1_2) * p!(num2)
+                    }
+                    [b'm', b'u', b'l', b'(', num1_1 @ b'0'..=b'9', num1_2 @ b'0'..=b'9', num1_3 @ b'0'..=b'9', b',', num2 @ b'0'..=b'9', b')', ..] => {
+                        sum += p!(num1_1, num1_2, num1_3) * p!(num2)
+                    }
+                    [b'm', b'u', b'l', b'(', num1 @ b'0'..=b'9', b',', num2_1 @ b'0'..=b'9', num2_2 @ b'0'..=b'9', b')', ..] =>
+                    {
+                        sum += p!(num1) * p!(num2_1, num2_2);
+                    }
+                    [b'm', b'u', b'l', b'(', num1_1 @ b'0'..=b'9', num1_2 @ b'0'..=b'9', b',', num2_1 @ b'0'..=b'9', num2_2 @ b'0'..=b'9', b')', ..] =>
+                    {
+                        sum += p!(num1_1, num1_2) * p!(num2_1, num2_2);
+                    }
+                    [b'm', b'u', b'l', b'(', num1_1 @ b'0'..=b'9', num1_2 @ b'0'..=b'9', num1_3 @ b'0'..=b'9', b',', num2_1 @ b'0'..=b'9', num2_2 @ b'0'..=b'9', b')', ..] =>
+                    {
+                        sum += p!(num1_1, num1_2, num1_3) * p!(num2_1, num2_2);
+                    }
+                    [b'm', b'u', b'l', b'(', num1 @ b'0'..=b'9', b',', num2_1 @ b'0'..=b'9', num2_2 @ b'0'..=b'9', num2_3 @ b'0'..=b'9', b')', ..] =>
+                    {
+                        sum += p!(num1) * p!(num2_1, num2_2, num2_3);
+                    }
+                    [b'm', b'u', b'l', b'(', num1_1 @ b'0'..=b'9', num1_2 @ b'0'..=b'9', b',', num2_1 @ b'0'..=b'9', num2_2 @ b'0'..=b'9', num2_3 @ b'0'..=b'9', b')', ..] =>
+                    {
+                        sum += p!(num1_1, num1_2) * p!(num2_1, num2_2, num2_3);
+                    }
+                    [b'm', b'u', b'l', b'(', num1_1 @ b'0'..=b'9', num1_2 @ b'0'..=b'9', num1_3 @ b'0'..=b'9', b',', num2_1 @ b'0'..=b'9', num2_2 @ b'0'..=b'9', num2_3 @ b'0'..=b'9', b')', ..] =>
+                    {
+                        let num1 = p!(num1_1, num1_2, num1_3);
+                        let num2 = p!(num2_1, num2_2, num2_3);
+                        debug!("{num1} * {num2}");
+                        sum += num1 * num2;
+                    }
+                    arr => {
+                        debug!(
+                            "Unexpected values: {:?}",
+                            std::str::from_utf8(&arr[..(min(arr.len(), 10))]).unwrap()
+                        )
+                    }
                 }
-                [b'd', b'o', b'n', b'\'', b't', b'(', b')', ..] if enabled => {
-                    enabled = false;
-                }
-                [b'm', b'u', b'l', b'(', num1 @ b'0'..=b'9', b',', num2 @ b'0'..=b'9', b')', ..]
-                    if enabled =>
-                {
-                    sum += p!(num1) * p!(num2);
-                }
-                [b'm', b'u', b'l', b'(', num1_1 @ b'0'..=b'9', num1_2 @ b'0'..=b'9', b',', num2 @ b'0'..=b'9', b')', ..]
-                    if enabled =>
-                {
-                    sum += p!(num1_1, num1_2) * p!(num2)
-                }
-                [b'm', b'u', b'l', b'(', num1_1 @ b'0'..=b'9', num1_2 @ b'0'..=b'9', num1_3 @ b'0'..=b'9', b',', num2 @ b'0'..=b'9', b')', ..]
-                    if enabled =>
-                {
-                    sum += p!(num1_1, num1_2, num1_3) * p!(num2)
-                }
-                [b'm', b'u', b'l', b'(', num1 @ b'0'..=b'9', b',', num2_1 @ b'0'..=b'9', num2_2 @ b'0'..=b'9', b')', ..]
-                    if enabled =>
-                {
-                    sum += p!(num1) * p!(num2_1, num2_2);
-                }
-                [b'm', b'u', b'l', b'(', num1_1 @ b'0'..=b'9', num1_2 @ b'0'..=b'9', b',', num2_1 @ b'0'..=b'9', num2_2 @ b'0'..=b'9', b')', ..]
-                    if enabled =>
-                {
-                    sum += p!(num1_1, num1_2) * p!(num2_1, num2_2);
-                }
-                [b'm', b'u', b'l', b'(', num1_1 @ b'0'..=b'9', num1_2 @ b'0'..=b'9', num1_3 @ b'0'..=b'9', b',', num2_1 @ b'0'..=b'9', num2_2 @ b'0'..=b'9', b')', ..]
-                    if enabled =>
-                {
-                    sum += p!(num1_1, num1_2, num1_3) * p!(num2_1, num2_2);
-                }
-                [b'm', b'u', b'l', b'(', num1 @ b'0'..=b'9', b',', num2_1 @ b'0'..=b'9', num2_2 @ b'0'..=b'9', num2_3 @ b'0'..=b'9', b')', ..]
-                    if enabled =>
-                {
-                    sum += p!(num1) * p!(num2_1, num2_2, num2_3);
-                }
-                [b'm', b'u', b'l', b'(', num1_1 @ b'0'..=b'9', num1_2 @ b'0'..=b'9', b',', num2_1 @ b'0'..=b'9', num2_2 @ b'0'..=b'9', num2_3 @ b'0'..=b'9', b')', ..]
-                    if enabled =>
-                {
-                    sum += p!(num1_1, num1_2) * p!(num2_1, num2_2, num2_3);
-                }
-                [b'm', b'u', b'l', b'(', num1_1 @ b'0'..=b'9', num1_2 @ b'0'..=b'9', num1_3 @ b'0'..=b'9', b',', num2_1 @ b'0'..=b'9', num2_2 @ b'0'..=b'9', num2_3 @ b'0'..=b'9', b')', ..]
-                    if enabled =>
-                {
-                    let num1 = p!(num1_1, num1_2, num1_3);
-                    let num2 = p!(num2_1, num2_2, num2_3);
-                    debug!("{num1} * {num2}");
-                    sum += num1 * num2;
-                }
-                arr => {
-                    debug!(
-                        "Unexpected values: {:?}",
-                        std::str::from_utf8(&arr[..(min(arr.len(), 10))]).unwrap()
-                    )
-                }
+            } else {
+                enabled = &input[partial_match_pos..][..4] == "do()".as_bytes();
             }
         }
 
@@ -240,5 +230,17 @@ mod test {
     fn p2_example() {
         let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
         assert_eq!(part2(input), 48);
+    }
+
+    const REAL_INPUT: &str = include_str!("../input/2024/day3.txt");
+
+    #[test]
+    fn p1_real() {
+        assert_eq!(part1(REAL_INPUT), 182_619_815);
+    }
+
+    #[test]
+    fn p2_real() {
+        assert_eq!(part2(REAL_INPUT), 80_747_545);
     }
 }
