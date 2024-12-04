a38557aef3a8e273b5073fa9b9e809962350d787
Tony Samuels <tonysamuels@protonmail.com>
2024-12-04 10:28:48 +0000

    chore: rename benches to include the day

--- benches/day4.rs
+++ benches/day4.rs
@@ -4,12 +4,16 @@ use aoc_2024::day4::{part1, part2};
 
 fn bench_part1(c: &mut Criterion) {
     let input = include_str!("../input/2024/day4.txt");
-    c.bench_function("part1", |b| b.iter(|| part1(black_box(input))));
+    c.bench_function(&format!("{}:part1", file!()), |b| {
+        b.iter(|| part1(black_box(input)))
+    });
 }
 
 fn bench_part2(c: &mut Criterion) {
     let input = include_str!("../input/2024/day4.txt");
-    c.bench_function("part2", |b| b.iter(|| part2(black_box(input))));
+    c.bench_function(&format!("{}:part2", file!()), |b| {
+        b.iter(|| part2(black_box(input)))
+    });
 }
 
 criterion_group!(benches, bench_part1, bench_part2);
