73d67f38bd86628c562910643c84889e1ac9a115
Tony Samuels <tonysamuels@protonmail.com>
2024-12-04 09:54:11 +0000

    perf(day4): simd finder

--- src/day4.rs
+++ src/day4.rs
@@ -1,5 +1,9 @@
+use std::simd::{cmp::SimdPartialEq, Simd};
+
 use aoc_runner_derive::aoc;
 use memchr::Memchr;
+
+use crate::debug;
 
 #[aoc(day4, part1)]
 pub fn part1(input: &str) -> u32 {
@@ -9,7 +13,7 @@ pub fn part1(input: &str) -> u32 {
 #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
 unsafe fn part1_inner<const LINE_LEN: usize>(input: &[u8]) -> u32 {
     let mut count = 0;
-    for x_pos in Memchr::new(b'X', input) {
+    for x_pos in Finder::new(b'X', input) {
         for diff in [
             // Right/left
             1,
@@ -43,7 +47,7 @@ pub fn part2(input: &str) -> u32 {
 #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
 unsafe fn part2_inner<const LINE_LEN: usize>(input: &[u8]) -> u32 {
     let mut count = 0;
-    for a_pos in Memchr::new(b'A', input) {
+    for a_pos in Finder::new(b'A', input) {
         let first = [
             input.get(a_pos.wrapping_sub(LINE_LEN + 1)),
             input.get(a_pos + LINE_LEN + 1),
@@ -63,6 +67,49 @@ unsafe fn part2_inner<const LINE_LEN: usize>(input: &[u8]) -> u32 {
     }
 
     count
+}
+
+struct Finder<'a> {
+    c: u8,
+    input: &'a [u8],
+    pos: usize,
+}
+
+impl<'a> Finder<'a> {
+    fn new(c: u8, input: &'a [u8]) -> Self {
+        Self { c, input, pos: 0 }
+    }
+}
+
+impl Iterator for Finder<'_> {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
+        unsafe fn inner(iter: &mut Finder<'_>) -> Option<usize> {
+            if iter.pos < iter.input.len() {
+                let line = Simd::<_, 64>::load_or(&iter.input[iter.pos..], Simd::splat(0));
+                let offset = line
+                    .simd_eq(Simd::splat(iter.c))
+                    .to_bitmask()
+                    .trailing_zeros() as usize;
+                debug!(
+                    "{}: {offset}",
+                    std::str::from_utf8(line.as_array()).unwrap()
+                );
+                iter.pos += offset + 1;
+                if offset == 64 {
+                    let _ = iter.next();
+                }
+
+                Some(iter.pos - 1)
+            } else {
+                None
+            }
+        }
+
+        unsafe { inner(self) }
+    }
 }
 
 #[cfg(test)]
@@ -100,5 +147,18 @@ MXMXAXMASX";
     #[test]
     fn p2_real() {
         assert_eq!(unsafe { part2(REAL_INPUT) }, 1_950);
+    }
+
+    #[test]
+    fn finder() {
+        let finder = &mut Finder::new(b'X', INPUT.as_bytes());
+        let memchr = &mut Memchr::new(b'X', INPUT.as_bytes());
+
+        for (count, (finder, memchr)) in finder.zip(&mut *memchr).enumerate() {
+            assert_eq!(finder, memchr, "Failed on {count}th time");
+        }
+
+        assert_eq!(finder.next(), None);
+        assert_eq!(memchr.next(), None);
     }
 }
