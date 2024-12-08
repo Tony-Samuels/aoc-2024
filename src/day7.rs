0a721b8567de77697da5f5a60714c28e858a8e94
Tony Samuels <tonysamuels@protonmail.com>
2024-12-08 05:52:49 +0000

    perf(day7): sort by least commonly hit conditions

--- src/day7.rs
+++ src/day7.rs
@@ -127,18 +127,18 @@ unsafe fn recurse_p2(target: u64, nums: &[u16; 12], index: usize) -> bool {
         num == target
     } else {
         (unchecked_rem(target, num) == 0 && recurse_p2(unchecked_div(target, num), nums, index - 1))
-            || (target >= num && recurse_p2(unchecked_sub(target, num), nums, index - 1)
-                || ({
-                    let tens = if num >= 100 {
-                        1_000
-                    } else if num >= 10 {
-                        100
-                    } else {
-                        10
-                    };
-                    unchecked_rem(target, tens) == num
-                        && recurse_p2(unchecked_div(target, tens), nums, index - 1)
-                }))
+            || (target >= num && recurse_p2(unchecked_sub(target, num), nums, index - 1))
+            || ({
+                let tens = if num >= 100 {
+                    1_000
+                } else if num >= 10 {
+                    100
+                } else {
+                    10
+                };
+                unchecked_rem(target, tens) == num
+                    && recurse_p2(unchecked_div(target, tens), nums, index - 1)
+            })
     }
 }
 
