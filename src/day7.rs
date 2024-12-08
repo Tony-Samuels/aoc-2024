b238829268a1e5fe9035198604646f1b842583ff
Tony Samuels <tonysamuels@protonmail.com>
2024-12-08 01:20:03 +0000

    perf(day7): minor tweaks that likely don't matter

--- src/day7.rs
+++ src/day7.rs
@@ -1,7 +1,7 @@
 use aoc_runner_derive::aoc;
 use atoi_simd::parse_any_pos;
 
-use crate::{assume, debug, ArrayVec, Assume};
+use crate::{ArrayVec, Assume};
 
 const EOL: u8 = b'\n';
 
@@ -11,40 +11,25 @@ pub fn part1(input: &str) -> u64 {
     unsafe fn inner(input: &str) -> u64 {
         let input = input.as_bytes();
         let mut count = 0;
-        let mut vec = ArrayVec::<20, u64>::new();
+        let mut vec = ArrayVec::<12, u64>::new();
         let mut pos = 0;
 
         while input.len() > pos + 3 {
             vec.clear();
 
             let (target, bytes) = parse_any_pos(input.get_unchecked(pos..)).assume();
-            assume!(
-                input[pos + bytes] == b':' && input[pos + bytes + 1] == b' ',
-                "Unexpected start of line terminators after {target}: {}{} ({}{})",
-                input[pos + bytes],
-                input[pos + bytes + 1],
-                input[pos + bytes] as char,
-                input[pos + bytes + 1] as char,
-            );
             pos += bytes + 2;
             loop {
                 let (next, bytes) = parse_any_pos(input.get_unchecked(pos..)).assume();
                 vec.push_unchecked(next);
                 let term = *input.get_unchecked(pos + bytes);
                 pos += bytes + 1;
-                assume!(
-                    term == b' ' || term == b'\n',
-                    "Unexpected terminator {term} ({})",
-                    term as char
-                );
                 if term == EOL {
                     break;
                 }
             }
 
-            debug!("Running with vec: {vec:?}");
             let math_checks_out = recurse_p1(target, vec);
-            debug!("Math checks out for {target}: {math_checks_out}");
             count += target * math_checks_out as u64;
         }
 
@@ -55,13 +40,10 @@ pub fn part1(input: &str) -> u64 {
 
 #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
 unsafe fn recurse_p1<const N: usize>(target: u64, mut nums: ArrayVec<N, u64>) -> bool {
-    if nums.len == 1 {
-        let num = nums.pop_unchecked();
-
+    let num = nums.pop_unchecked();
+    if nums.len == 0 {
         num == target
     } else {
-        let num = nums.pop_unchecked();
-
         (target % num == 0 && recurse_p1(target / num, nums))
             || (target >= num && recurse_p1(target - num, nums))
     }
@@ -73,44 +55,27 @@ pub fn part2(input: &str) -> u64 {
     unsafe fn inner(input: &str) -> u64 {
         let input = input.as_bytes();
         let mut count = 0;
-        let mut vec = ArrayVec::<20, u64>::new();
+        let mut vec = ArrayVec::<12, u64>::new();
         let mut pos = 0;
 
         while input.len() > pos + 3 {
             vec.clear();
 
             let (target, bytes) = parse_any_pos(input.get_unchecked(pos..)).assume();
-            assume!(
-                input[pos + bytes] == b':' && input[pos + bytes + 1] == b' ',
-                "Unexpected start of line terminators after {target}: {}{} ({}{})",
-                input[pos + bytes],
-                input[pos + bytes + 1],
-                input[pos + bytes] as char,
-                input[pos + bytes + 1] as char,
-            );
             pos += bytes + 2;
             loop {
                 let (next, bytes) = parse_any_pos(input.get_unchecked(pos..)).assume();
                 vec.push_unchecked(next);
                 let term = *input.get_unchecked(pos + bytes);
                 pos += bytes + 1;
-                assume!(
-                    term == b' ' || term == b'\n',
-                    "Unexpected terminator {term} ({})",
-                    term as char
-                );
                 if term == EOL {
                     break;
                 }
             }
 
-            debug!("Running with vec: {vec:?}");
             let math_checks_out = recurse_p2(target, vec);
-            debug!("Math checks out for {target}: {math_checks_out}");
             count += target * math_checks_out as u64;
         }
-
-        debug!("Remaining input: {}", input.len());
 
         count
     }
@@ -119,12 +84,10 @@ pub fn part2(input: &str) -> u64 {
 
 #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
 unsafe fn recurse_p2<const N: usize>(target: u64, mut nums: ArrayVec<N, u64>) -> bool {
-    if nums.len == 1 {
-        let num = nums.pop_unchecked();
+    let num = nums.pop_unchecked();
+    if nums.len == 0 {
         num == target
     } else {
-        let num = nums.pop_unchecked();
-
         (target % num == 0 && recurse_p2(target / num, nums))
             || (target >= num && recurse_p2(target - num, nums)
                 || ({
